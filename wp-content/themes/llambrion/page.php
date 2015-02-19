a2d3342aa2931fc35d7badc102d23ac7357287a8
Jesus Domenech <jesusjda@gmail.com>
2015-02-19 10:55:34 +0100

    principal page

--- wp-content/themes/llambrion/page.php
+++ wp-content/themes/llambrion/page.php
@@ -1,5 +1,6 @@
 <?php
 get_header(); 
+the_post();
 ?>
 
 <div class="fluid-container espacio-arriba-30">
@@ -9,9 +10,34 @@ get_header();
 			<div class="col-xs-12">
 				<h1 class="page-title"><?php echo get_the_title(); ?></h1>
 			</div>
-
 			<div class="col-xs-12">
-				<p><?php echo get_field('content'); ?></p>
+				<?php
+					$query=new WP_Query( array ( 'post_type' => 'post', 'order' => 'DESC' ) );
+					while ( $query->have_posts() ){
+						$query->the_post();
+						?>
+							<div class="col-xs-12 panel-info">
+								<div class="panel-heading">
+									<h3 class="panel-title">
+										<a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
+									</h3>
+								</div>
+								<div class="panel-body fondo-blanco">
+									<div class="row">
+										<div class="col-sm-12">
+											<?php the_excerpt(); ?>
+											<a href="<?php echo get_permalink(); ?>">
+												<button class="btn btn-color"><?php _e('M&aacutes', 'joseweb'); ?></button>
+											</a>
+										</div>
+									</div>
+								</div>
+							</div><br/>
+						<?php
+					}
+					wp_reset_query();
+				?>
+
 			</div>
 
 		</div>
