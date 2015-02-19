9dbe1f5d7dbd06cd51930e6505831eed0cdc0854
Jesus Domenech <jesusjda@gmail.com>
2015-02-19 11:30:25 +0100

    Single good

--- wp-content/themes/llambrion/single.php
+++ wp-content/themes/llambrion/single.php
@@ -11,27 +11,19 @@ the_post();
 <div class="fluid-container espacio-arriba-20">
 	<div class="row">
 		<div class="col-sm-8 col-xs-12">
-			
-			<div class="col-xs-12">
-				<h1 class="page-title"><?php echo get_the_title(); ?></h1>
-				<br/>
+			<div class="col-xs-8">
+				<h1 class="page-title">
+					<a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
+				</h1>
 			</div>
+			<div class="col-xs-3 pull-rigth">
+				<h1 class="pull-rigth">
+					<small><?php echo get_the_date(); ?></small>
+				</h1>
+			</div>
+
 			<div class="col-xs-12">
 				<div class="col-xs-12 panel-info">
-					<div class="panel-heading">
-						<div class="row">
-							<div class="col-xs-8">
-								<h3 class="panel-title">
-									<a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
-								</h3>
-							</div>
-							<div class="col-xs-3 pull-rigth">
-								<h3 class="pull-rigth">
-									<small><?php echo get_the_date(); ?></small>
-								</h3>
-							</div>
-						</div>
-						</div>
 						<div class="panel-body fondo-blanco">
 							<div class="row">
 								<div class="col-sm-12">
