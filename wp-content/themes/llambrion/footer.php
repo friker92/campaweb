4eb19039bc98917a7a93d33cc7c9b30feba4255b
Jesus Domenech <jesusjda@gmail.com>
2015-02-24 21:17:13 +0100

    footer

--- wp-content/themes/llambrion/footer.php
+++ wp-content/themes/llambrion/footer.php
@@ -7,14 +7,29 @@
 			<footer id="footer-content" class="site-footer fondo-negro espacio-arriba-30" >
 
 				<div class="row"><br/>
-					<div class="col-xs-offset-6 col-xs-6">
-					  <h4>Contacto</h4>
-					  <ul>
-					    <li>Sobre la web: <a onclick="p1='o:web';p2='llambrion.com'; this.href='mailt' + p1 + '@' + p2">web@llambrion.com</a></li>
-					    <li>Sobre Llambrión: <a onclick="p1='o:web';p2='llambrion.com'; this.href='mailt' + p1 + '@' + p2">contacto@llambrion.com</a></li>
-					  </ul>
-					</div>
-					<br/><br/>
+				  <div class="col-xs-6">
+				    <h4>Agradecimientos</h4>
+				    <div class="col-xs-6">
+				      <a href="http://fundacionjesusysanmartin.blogspot.com.es/">
+					<img src="http://www.llambrion.com/wp-content/uploads/2015/02/img_Fundacion-Jesus-San-Martin-copia.png" />
+					<span>Fundación Jesús Y San Martín</span>
+				      </a>
+				    </div>
+				    <div class="col-xs-6">
+				      <a href="http://www.diocesisgetafe.es/">
+					<img src="http://www.llambrion.com/wp-content/uploads/2015/02/img_Fundacion-Jesus-San-Martin-copia.png" />
+					<span>Diócesis de Getafe</span>
+				      </a>
+				    </div>
+				  </div>
+				  <div class="col-xs-6">
+				    <h4>Contacto</h4>
+				    <ul>
+				      <li>Sobre la web: <a onclick="p1='o:web';p2='llambrion.com'; this.href='mailt' + p1 + '@' + p2">web@llambrion.com</a></li>
+				      <li>Sobre Llambrión: <a onclick="p1='o:web';p2='llambrion.com'; this.href='mailt' + p1 + '@' + p2">contacto@llambrion.com</a></li>
+				    </ul>
+				  </div>
+				  <br/><br/>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
