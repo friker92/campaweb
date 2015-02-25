c5445199fec1c5a23fac21b09eb71935e1b6a98e
Jesus Domenech <jesusjda@gmail.com>
2015-02-26 00:31:57 +0100

    footer mobile

--- wp-content/themes/llambrion/footer.php
+++ wp-content/themes/llambrion/footer.php
@@ -6,34 +6,34 @@
 
 			<footer id="footer-content" class="site-footer fondo-negro espacio-arriba-30" >
 				<div class="row">
-					<div class="col-xs-6">
+					<div class="col-xs-6 footer-column espacio-arriba-20">
 						<h4 class="h4-footer">Agradecimientos</h4>
-						<div class="col-xs-6">
+						<div class="col-sm-6 col-xs-12">
 							<div class="col-xs-12">
 								<a href="http://fundacionjesusysanmartin.blogspot.com.es/" class="footer-link img">
 									<img src="http://www.llambrion.com/wp-content/uploads/2015/02/img_Fundacion-Jesus-San-Martin-copia.png" />
 								</a>
 							</div>
-							<div class="col-xs-12">
+							<div class="col-xs-12 espacio-arriba-20">
 								<a href="http://fundacionjesusysanmartin.blogspot.com.es/" class="footer-link span">
 									<span>Fundación Jesús Y San Martín</span>
 								</a>
 							</div>
 						</div>
-						<div class="col-xs-6">
+						<div class="col-sm-6 col-xs-12 espacio-arriba-20-xs">
 							<div class="col-xs-12">
 								<a href="http://www.diocesisgetafe.es/" class="footer-link img">
 									<img src="http://www.llambrion.com/wp-content/uploads/2015/02/getafe.png" />
 								</a>
 							</div>
-							<div class="col-xs-12">
+							<div class="col-xs-12 espacio-arriba-20">
 								<a href="http://www.diocesisgetafe.es/" class="footer-link span">
 									<span>Diócesis de Getafe</span>
 								</a>
 							</div>
 						</div>
 					</div>
-					<div class="col-xs-6">
+					<div class="col-xs-6 footer-column espacio-arriba-20">
 						<h4 class="h4-footer">Contacto</h4>
 						<ul id="contacto-footer">
 							<li>Sobre la web: <a onclick="p1='o:web';p2='llambrion.com'; this.href='mailt' + p1 + '@' + p2">web@llambrion.com</a></li>
